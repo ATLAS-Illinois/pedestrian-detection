abf7f5946bda834ec26ade997a310c5e20034418
yaswant2403 <yse2@illiois.edu>
2023-06-22 20:06:10 -0500

    Added environment.yml and fixed counter

--- backend/run_inference.py
+++ backend/run_inference.py
@@ -9,8 +9,8 @@ image_files = [os.path.join("./data/video/frames/", f) for f in os.listdir("./da
 num_people = 0
 
 for image in image_files:
-    results = model(image, classes=0)
-    num_people = results[0].boxes.data.size()[0]
+    results = model(image, classes=0, imgsz=1280)
+    num_people += results[0].boxes.data.size()[0]
 
 print(f"In a 20 second clip of the livestream, YOLOv8 found {num_people} people!")
     
