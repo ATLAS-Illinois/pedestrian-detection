b788cf96c6acc71f42dde7e5263c70357b06624f
yaswant2403 <yse2@illiois.edu>
2023-07-06 17:05:14 -0500

    Removed unnecessary code in run_inference

--- backend/run_inference.py
+++ backend/run_inference.py
@@ -1,23 +1,7 @@
-# from flask import Flask, render_template
 from ultralytics import YOLO
 import os
 
-# app = Flask(__name__)
-
 model = YOLO("yolov8n.pt")
-# @app.route('/')
-# def home():
-#     model = YOLO("yolov8n.pt")
-#     image_files = [os.path.join("./data/video/frames/", f) for f in os.listdir("./data/video/frames/")]
-
-#     # counter for number of people
-#     num_people = 0
-
-#     for image in image_files:
-#         results = model(image, classes=0, imgsz=1280)
-#         num_people += results[0].boxes.data.size()[0]
-    
-#     return render_template('./index.html', num_people=num_people)
 
 def predict_people():
     image_files = [os.path.join("./data/video/frames/", f) for f in os.listdir("./data/video/frames/")]
@@ -29,7 +13,3 @@ def predict_people():
         num_people += results[0].boxes.data.size()[0]
     
     return num_people
-
-# if __name__ == '__main__':
-#     app.run()
-    
