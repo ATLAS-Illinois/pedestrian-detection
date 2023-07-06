129228468892ec54a7b89db81faed97f9f99f120
yaswant2403 <yse2@illiois.edu>
2023-07-06 16:30:11 -0500

    Added Flask app with better frontend displaying histogram

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from run_inference import predict_people
+from flask import Flask, render_template, redirect
+
+num_people = predict_people()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():    
+    return render_template('./index.html', num_people=num_people)
+
+
+if __name__ == '__main__':
+    app.run()
+    
